32129f86873ad78f77d605ea43bd27aeb4940d4b
K. M. Merajul Arefin <merajularefin@gmail.com>
2022-09-25 20:02:24 +0600

    little cleanup

--- app/src/main/java/com/example/trymaya/ui/plan/PlanFragment.java
+++ app/src/main/java/com/example/trymaya/ui/plan/PlanFragment.java
@@ -29,16 +29,13 @@ import com.example.trymaya.databinding.FragmentPlanBinding;
 
 import java.util.Calendar;
 
-public class PlanFragment extends Fragment implements View.OnClickListener {
+public class PlanFragment extends Fragment {
     private FragmentPlanBinding binding;
-    Activity activity = getActivity();
-    Button btnDatePicker,btnTimePicker;
+    Button btnDatePicker, btnTimePicker;
 
     EditText txtDate, txtTime;
     int mYear, mMonth, mDay, mHour, mMinute;
-    long  givenTime=0;
-    long givenDate=0;
-    long totalTime=0;
+    long totalTime = 0;
     int ElapsedTime;
     long tot;
 
@@ -49,18 +46,17 @@ public class PlanFragment extends Fragment implements View.OnClickListener {
 
         binding = FragmentPlanBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-        super.onCreateView(inflater,container,savedInstanceState);
-
+        super.onCreateView(inflater, container, savedInstanceState);
 
 
         View v = inflater.inflate(R.layout.fragment_plan, container, false);
-        btnDatePicker = (Button) v.findViewById(R.id.button1);
-        btnTimePicker = (Button) v.findViewById(R.id.btn_time);
-        txtDate = (EditText) v.findViewById(R.id.in_date);
-        txtTime = (EditText) v.findViewById(R.id.in_time);
+        btnDatePicker = v.findViewById(R.id.button1);
+        btnTimePicker = v.findViewById(R.id.btn_time);
+        txtDate = v.findViewById(R.id.in_date);
+        txtTime = v.findViewById(R.id.in_time);
 
-        btnDatePicker.setOnClickListener(this);
-        btnTimePicker.setOnClickListener(this);
+        btnDatePicker.setOnClickListener(this::onDatePickerClick);
+        btnTimePicker.setOnClickListener(this::onTimePickerClick);
 
         createNotificationChannel();
 
@@ -68,25 +64,27 @@ public class PlanFragment extends Fragment implements View.OnClickListener {
 
         button.setOnClickListener(view -> {
 
-            Toast.makeText(getActivity(),"Reminder is set", Toast.LENGTH_SHORT).show();
+            Toast.makeText(getActivity(), "Reminder is set", Toast.LENGTH_SHORT).show();
 //Toast.makeText(this, "Reminder is set" , Toast.LENGTH_SHORT).show();
-            Intent intent = new Intent(PlanFragment.this.getActivity(),ReminderBroadcast.class);
-            PendingIntent pendingIntent = PendingIntent.getBroadcast(getContext(), 0, intent, PendingIntent.FLAG_IMMUTABLE);
+            Intent intent = new Intent(PlanFragment.this.getActivity(), ReminderBroadcast.class);
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(getContext(), 0,
+                    intent, PendingIntent.FLAG_IMMUTABLE);
 
-            AlarmManager alarmManager = (AlarmManager) getContext().getSystemService(Context.ALARM_SERVICE);
+            AlarmManager alarmManager = (AlarmManager) requireContext()
+                    .getSystemService(Context.ALARM_SERVICE);
 
 
             long timeAtButtonClick = System.currentTimeMillis();
-            long tenSecondsInMillis = 7*86400*1000  - ElapsedTime - totalTime;
+            long tenSecondsInMillis = 7 * 86400 * 1000 - ElapsedTime - totalTime;
             tot = tenSecondsInMillis;
 
             alarmManager.set(AlarmManager.RTC_WAKEUP, timeAtButtonClick + tenSecondsInMillis, pendingIntent);
         });
-return v;
+        return v;
 
-       // final TextView textView = binding.textPlan;
+        // final TextView textView = binding.textPlan;
         //planViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-       // return root;
+        // return root;
     }
 
     private void createNotificationChannel() {
@@ -98,7 +96,8 @@ return v;
             channel.setDescription(description);
             // Register the channel with the system; you can't change the importance
             // or other notification behaviors after this
-            NotificationManager notificationManager = getContext().getSystemService(NotificationManager.class);
+            NotificationManager notificationManager = requireContext()
+                    .getSystemService(NotificationManager.class);
             notificationManager.createNotificationChannel(channel);
         }
     }
@@ -110,66 +109,52 @@ return v;
         binding = null;
     }
 
-    @Override
-    public void onClick(View v) {
+    public void onDatePickerClick(View v) {
 
-        if (v == btnDatePicker) {
-
-            // Get Current Date
-            final Calendar c = Calendar.getInstance();
-            mYear = c.get(Calendar.YEAR);
-            mMonth = c.get(Calendar.MONTH);
-            mDay = c.get(Calendar.DAY_OF_MONTH);
+        // Get Current Date
+        final Calendar c = Calendar.getInstance();
+        mYear = c.get(Calendar.YEAR);
+        mMonth = c.get(Calendar.MONTH);
+        mDay = c.get(Calendar.DAY_OF_MONTH);
 
 
-            DatePickerDialog datePickerDialog = new DatePickerDialog(getActivity(),
-                    new DatePickerDialog.OnDateSetListener() {
+        DatePickerDialog datePickerDialog = new DatePickerDialog(getActivity(),
+                (view, year, monthOfYear, dayOfMonth) -> {
 
-                        @Override
-                        public void onDateSet(DatePicker view, int year,
-                                              int monthOfYear, int dayOfMonth) {
+                    txtDate.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
 
-                            txtDate.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+                    Calendar cal = Calendar.getInstance();
+                    int yearr = cal.get(Calendar.YEAR);
+                    int monthh = cal.get(Calendar.MONTH);
+                    int day = cal.get(Calendar.DATE);
+                    int currentIndex = (year - 1970) * 12 + monthh;
+                    ElapsedTime = day - dayOfMonth;
+                    ElapsedTime = ElapsedTime * 86400 * 1000;
+                    //txtDate.setText(ElapsedTime +":");
+                }, mYear, mMonth, mDay);
+        datePickerDialog.show();
+    }
 
-                            Calendar cal = Calendar.getInstance();
-                            int yearr = cal.get(Calendar.YEAR);
-                            int monthh = cal.get(Calendar.MONTH);
-                            int day = cal.get(Calendar.DATE);
-                            int currentIndex = (year - 1970) * 12 + monthh;
-                            ElapsedTime = day-dayOfMonth;
-                            ElapsedTime = ElapsedTime*86400*1000;
-                            //txtDate.setText(ElapsedTime +":");
-                        }
-                    }, mYear, mMonth, mDay);
-            datePickerDialog.show();
-        }
-        if (v == btnTimePicker) {
+    public void onTimePickerClick(View v) {
+        // Get Current Time
+        final Calendar c = Calendar.getInstance();
+        mHour = c.get(Calendar.HOUR_OF_DAY);
+        mMinute = c.get(Calendar.MINUTE);
 
-            // Get Current Time
-            final Calendar c = Calendar.getInstance();
-            mHour = c.get(Calendar.HOUR_OF_DAY);
-            mMinute = c.get(Calendar.MINUTE);
+        // Launch Time Picker Dialog
+        TimePickerDialog timePickerDialog = new TimePickerDialog(getActivity(),
+                (view, hourOfDay, minute) -> {
 
-            // Launch Time Picker Dialog
-            TimePickerDialog timePickerDialog = new TimePickerDialog(getActivity(),
-                    new TimePickerDialog.OnTimeSetListener() {
+                    //txtTime.setText(hourOfDay + ":" + minute);
 
-                        @Override
-                        public void onTimeSet(TimePicker view, int hourOfDay,
-                                              int minute) {
-
-                            //txtTime.setText(hourOfDay + ":" + minute);
-
-                            // txtTime.setText(ElapsedTime +":");
-                            Calendar no = Calendar.getInstance();
-                            int hourN = no.get(Calendar.HOUR_OF_DAY);
-                            int minuteN = no.get(Calendar.MINUTE);
-                            totalTime = minuteN - minute;
-                            totalTime = totalTime*60*1000;
-                            txtTime.setText(tot +":");
-                        }
-                    }, mHour, mMinute, false);
-            timePickerDialog.show();
-        }
+                    // txtTime.setText(ElapsedTime +":");
+                    Calendar no = Calendar.getInstance();
+                    int hourN = no.get(Calendar.HOUR_OF_DAY);
+                    int minuteN = no.get(Calendar.MINUTE);
+                    totalTime = minuteN - minute;
+                    totalTime = totalTime * 60 * 1000;
+                    txtTime.setText(tot + ":");
+                }, mHour, mMinute, false);
+        timePickerDialog.show();
     }
 }
